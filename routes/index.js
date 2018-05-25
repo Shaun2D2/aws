8fa3f9f7bba5560a846e7c4be33e28531949e6fb
Shaun Lobisnger <slobsinger@godaddy.com>
2018-05-25 11:40:11 -0700

    adding stuff...

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.status(200).json({ status: 'OK', message: 'Systems are so ready!' });
+  res.status(200).json({ status: 'OK', message: 'Systems are so ready!', users: ['jim', 'joe', 'john'] });
 });
 
 export default router;
