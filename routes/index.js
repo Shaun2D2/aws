9353944e87e539a9f6e17b19f2e1d67d37b79d66
Shaun Lobisnger <Shaun.lobsinger@gmail.com>
2018-05-28 15:59:47 -0700

    added buildspec and updated route for fun

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.status(200).json({ status: 'OK', message: 'Systems are so ready!', users: ['jim', 'joe', 'john'] });
+  res.status(200).json({
+      rocket: 'falcon 1',
+      status: 'OK',
+      passengers: ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+  });
 });
 
 export default router;
