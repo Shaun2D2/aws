8f1e825967d3879b0fd9dd23db950396d0a2a52e
Shaun <shaun.lobsinger@gmail.com>
2018-05-30 12:17:58 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.status(200).json({
-      rocket: 'falcon heavy',
+      rocket: 'falcon 1',
       status: 'OK',
       passengers: ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
   });
